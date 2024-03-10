0dc8f307651c1ef05d397952fb95b7d8f712b5ec
ransboak <66210638+ransboak@users.noreply.github.com>
2024-03-09 22:41:06 -0800

    Updates

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -48,6 +49,7 @@ Route::get('/faqs', [PageController::class, 'faqsPage'])->name('faqs');
 
 
 
+
 Route::get('/dashboard', function () {
     return view('backend.pages.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -56,6 +58,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/addBlog', [BlogController::class, 'addBlog'])->name('addBlog');
 });
 
 require __DIR__.'/auth.php';
